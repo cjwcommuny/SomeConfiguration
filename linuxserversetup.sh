65176ae1b8db0872bb38736b27d492df520d2182
cjwcommuny <cjwcommuny@outlook.com>
2018-11-26 12:35:23 +0800

    remove linuxserver.sh

--- linuxserversetup.sh
+++
@@ -1,8 +0,0 @@
-#this is shell script for ubuntu server setup
-#!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-#install oh-my-zsh
-sudo apt install zsh
-chsh -s /bin/zsh # 将 Zsh 设置为默认 Shell
